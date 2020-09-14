6027a858861fac80f52680b6e247ebe3caa8b8c7
Avery Burns <a.aburns95@gmail.com>
2020-09-14 00:27:16 -0500

    cards.js cardMaker functions, event listener onclick functions, MVP complete

--- components/Cards.js
+++ components/Cards.js
@@ -14,8 +14,8 @@
 //       <img src={url of authors image} />
 //     </div>
 //     <span>By {author's name}</span>
-//   </div>
-// </div>
+//   </div><--author div closes-->
+// </div><-- card div closes-->
 //
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
@@ -31,29 +31,63 @@ axios.get('https://lambda-times-api.herokuapp.com/articles')
 
 
         for (const [key, value] of Object.entries(articleDOM)) {
-            console.log(`${key}: [{${value[0]}}]`);
-          };
+            let i = 0;
+            const keyForeach = key;
+            const valArrayObj = value;
+
+            console.log([keyForeach,valArrayObj]);
+            valArrayObj.forEach( (element) => {
+                cardMaker(element);
+                
+            })
+            i++ ;
+          }
         
     })
     .catch( err => {
         console.log(err)
     })
 
-// const cardMaker = (object) => {
-//     // create elements
-//     const cardDiv = document.createElement('div');
-//     const headDiv = document.createElement('div');
-//     const authDiv = document.createElement('div');
-//     const imgCont = document.createElement('div');
-//     const imgDOM = document.createElement('img');
-//     const authSpan = document.createElement('span');
+// component function for card DOM element
+const cardMaker = (object) => {
+    // create elements
+    const cardDiv = document.createElement('div');
+    const headDiv = document.createElement('div');
+    const authDiv = document.createElement('div');
+    const imgCont = document.createElement('div');
+    const imgDOM = document.createElement('img');
+    const authSpan = document.createElement('span');
 
-//     // assign class vals
-//     cardDiv.classList.add('card');
-//     headDiv.classList.add('headline');
-//     authDiv.classList.add('author');
-//     imgCont.classList.add('img-container');
+    // assign class vals
+    cardDiv.classList.add('card');
+    headDiv.classList.add('headline');
+    authDiv.classList.add('author');
+    imgCont.classList.add('img-container');
 
-//     // img src
-//     imgDOM.src = 
-// }
+    // img src
+    imgDOM.src = object.authorPhoto;
+
+    // text content
+    headDiv.textContent = object.headline;
+    authSpan.textContent = object.authorName;
+
+    // append children to cardDiv
+    cardDiv.appendChild(headDiv);
+    cardDiv.appendChild(authDiv);
+    authDiv.appendChild(imgCont);
+    imgCont.appendChild(imgDOM);
+    authDiv.appendChild(authSpan);
+
+    // event listener
+    cardDiv.addEventListener("click", (event) => {
+        console.log(headDiv.textContent)
+    })
+
+    // append cardDiv to DOM
+    // get
+    const cardsCont = document.querySelector('.cards-container');
+    // append
+    cardsCont.appendChild(cardDiv);
+
+
+}
