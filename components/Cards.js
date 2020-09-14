3e4203d7f307183eba9ea1121e22af930b39ab3b
Avery Burns <a.aburns95@gmail.com>
2020-09-13 22:47:58 -0500

    cards.js simple GET req, incomplete cardMaker component created

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,40 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+const axios = require('axios');
+
+axios.get('https://lambda-times-api.herokuapp.com/articles')
+    .then( result => {
+        const articleDOM = result.data.articles;
+       
+        console.log(articleDOM);
+
+
+        for (const [key, value] of Object.entries(articleDOM)) {
+            console.log(`${key}: [{${value[0]}}]`);
+          };
+        
+    })
+    .catch( err => {
+        console.log(err)
+    })
+
+// const cardMaker = (object) => {
+//     // create elements
+//     const cardDiv = document.createElement('div');
+//     const headDiv = document.createElement('div');
+//     const authDiv = document.createElement('div');
+//     const imgCont = document.createElement('div');
+//     const imgDOM = document.createElement('img');
+//     const authSpan = document.createElement('span');
+
+//     // assign class vals
+//     cardDiv.classList.add('card');
+//     headDiv.classList.add('headline');
+//     authDiv.classList.add('author');
+//     imgCont.classList.add('img-container');
+
+//     // img src
+//     imgDOM.src = 
+// }
